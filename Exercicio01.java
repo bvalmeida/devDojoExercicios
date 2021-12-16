b74661066b2f3276c53877defbd606ba74c40108
Breno Vieira <bvalmeida94@gmail.com>
2021-12-16 10:54:45 -0300

    Primeiro Commit

---
+++ Exercicio01.java
@@ -0,0 +1,23 @@
+package deveDojoExercicios;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        /*
+         * Prática DeveDojo
+         * Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte
+         * mensagem:
+         * Eu <nome>, morando no endereço <endereço>,
+         * confirmo que recebi o salário de <salario>, na data <data>
+         */
+
+        String nome = "Breno Vieira";
+        String endereco = "Rua Antônio Boró";
+        float salario = 5493.32f;
+        String dataDoSalario = "01/02/2022";
+
+        System.out.println("Eu " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salário de "
+                + salario + ", na data " + dataDoSalario);
+
+    }
+
+}
